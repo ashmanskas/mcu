510755512656f25cb1b8ab1ce465ffa1af06ef49
Max Cohen <maxcohen@petdaq.HEP.upenn.edu>
2022-06-15 13:55:27 -0400

    update run.sh to look for cocotb-config in /opt/anaconda3/bin

--- cocotb/run.sh
+++ cocotb/run.sh
@@ -1,6 +1,9 @@
 if [ -e $HOME/.local/bin/cocotb-config ]; then 
   export PATH=$HOME/.local/bin:$PATH; 
 fi
+if [ -e /opt/anaconda3/bin/cocotb-config ]; then
+  export PATH=/opt/anaconda3/bin:$PATH;
+fi
 export COCOTB_REDUCED_LOG_FMT=1
 export PYTHONDONTWRITEBYTECODE=1
 export IVERILOG_DUMPER=lxt2
